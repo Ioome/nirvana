a90e1bfff2ce90b897efbfe7ebf396bb2b63ea5c
devil <50450947+Ioome@users.noreply.github.com>
2022-10-15 14:53:00 +0800

    sutton
    增加目录

---
+++ permissions/src/main/java/com/sutton/permissions/Main.java
@@ -0,0 +1,13 @@
+package com.sutton.permissions;
+
+/**
+ * @author loome
+ * @version 1.0.1
+ * @date
+ * @description this is a java file
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
