93127b384c8c55cf3b79bf5f60c18c487a9d90bb
devil <50450947+Ioome@users.noreply.github.com>
2022-10-15 11:05:52 +0800

    sutton
    Test 删除文件夹，并将修改合并到main

--- working/design/src/sutton-service-provider/src/structure/Adapter/V5Power.java
+++
@@ -1,4 +0,0 @@
-
-public interface V5Power {
-    public int provideV5Power();
-}   
--- working/design/src/sutton-service-provider/src/structure/Adapter/v220power.java
+++
@@ -1,7 +0,0 @@
-
-public class v220power {
-    public int provideV220Power() {
-        Log.e("---", "现有类: 我们提供的是220v的家用电");
-        return 220;
-    }
-}
