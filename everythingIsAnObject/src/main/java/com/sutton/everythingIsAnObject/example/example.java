b8ab32ec9f88e2eff25b6be4c97ca08dfe0d937f
devil <50450947+Ioome@users.noreply.github.com>
2022-10-15 17:03:45 +0800

    sutton
    增加 知识点

---
+++ everythingIsAnObject/src/main/java/com/sutton/everythingIsAnObject/example/example.java
@@ -0,0 +1,19 @@
+package com.sutton.everythingIsAnObject.example;
+
+import com.sutton.everythingIsAnObject.Main;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @version 1.0.1
+ * @program: nirvana
+ * @description: 例子
+ * @author: Mr.wang.sutton
+ * @create: 2022-10-15 16:42
+ **/
+public class example {
+    public static void main(String[] args) {
+        String s;
+        //System.out.println(s.getBytes(StandardCharsets.UTF_8));
+    }
+}
