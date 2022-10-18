b7811ca0333e7fb68b4d8940fb58e8cc9b1d06a1
sutton <50450947+Ioome@users.noreply.github.com>
2022-10-18 13:08:09 +0800

    sutton

---
+++ accessToTheIndicator/src/main/java/org/example/Dinner.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.example.dessert.Cookie;
+
+/**
+ * @version 1.0.1
+ * @program: nirvana
+ * @description: 晚饭
+ * @author: Mr.wang.sutton
+ * @create: 2022-10-17 20:37
+ **/
+public class Dinner {
+    public Dinner() {
+        System.out.println("Dinner constructor");
+    }
+
+    public static void main(String[] args) {
+        Cookie cookie=new Cookie();
+//        cookie.foo();
+    }
+}
---
+++ accessToTheIndicator/src/main/java/org/example/dessert/Cookie.java
@@ -0,0 +1,18 @@
+package org.example.dessert;
+
+/**
+ * @version 1.0.1
+ * @program: nirvana
+ * @description: Cookie
+ * @author: Mr.wang.sutton
+ * @create: 2022-10-17 20:36
+ **/
+public class Cookie {
+    public Cookie() {
+        System.out.println("Cookie constructor");
+    }
+
+    void foo() {
+        System.out.println("foo");
+    }
+}
