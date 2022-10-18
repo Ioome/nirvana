22e4cc69b5ad551c2d70063ded2ec53070904a8c
sutton <50450947+Ioome@users.noreply.github.com>
2022-10-18 14:33:43 +0800

    Update useForEach.java

--- control/src/main/java/com/sutton/control/practice/useForEach.java
+++ control/src/main/java/com/sutton/control/practice/useForEach.java
@@ -1,17 +1,35 @@
 package com.sutton.control.practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @version 1.0.1
  * @program: nirvana
  * @description: 使用 Foreach
- *  使用 foreach
+ * 使用 foreach
+ *  for(Iterator i$ = a.iterator(); i$.hasNext(); System.out.print(temp)){
+ *     temp = (String)i$.next();
+ * }
  *
+ * String arr[] = { "1", "2"  };
+ *   String arr$[] = arr;
+ *   int len$ = arr$.length;
+ *   for(int i$ = 0; i$ < len$; i$++)
+ *   {
+ *       String e = arr$[i$];
+ *       System.out.println(e);
+ *   }
  * @author: Mr.wang.sutton
  * @create: 2022-10-18 14:23
  **/
 public class useForEach {
 
     public static void main(String[] args) {
-
+        List<String> list;
+        list = new ArrayList<String>();
+        for (String e : list) {
+            list.add("1");
+        }
     }
 }
