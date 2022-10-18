f8bbb331a54a48a0e70c13bd7b5f3ffceaeb896a
sutton <50450947+Ioome@users.noreply.github.com>
2022-10-18 13:39:50 +0800

    sutton
    速度

---
+++ operator/src/main/java/com/sutton/operator/practice/usePractice4.java
@@ -0,0 +1,27 @@
+package com.sutton.operator.practice;
+
+/**
+ * @version 1.0.1
+ * @program: nirvana
+ * @description: 计算速度的程序
+ * @author: Mr.wang.sutton
+ * @create: 2022-10-18 13:34
+ **/
+public class usePractice4 {
+
+    /**
+     * 距离
+     */
+    static double distance = 20.0;
+
+    /**
+     * 时间
+     */
+    static double time = 2;
+
+    public static void main(String[] args) {
+        double speed = distance / time;
+        System.out.println(speed);
+    }
+}
+
