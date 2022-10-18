a9d6fe1e8644b60163928ef1d3592f12f9ca8d35
sutton <50450947+Ioome@users.noreply.github.com>
2022-10-18 14:08:31 +0800

    sutton
    产生随机数

---
+++ control/src/main/java/com/sutton/control/practice/usePrintNumber.java
@@ -0,0 +1,17 @@
+package com.sutton.control.practice;
+
+/**
+ * @version 1.0.1
+ * @program: nirvana
+ * @description: 打印 1 到 100
+ * @author: Mr.wang.sutton
+ * @create: 2022-10-18 13:59
+ **/
+public class usePrintNumber {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
