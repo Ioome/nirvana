96f8ec804ec2f121a34f5b2f440f4c7066ea7ce8
sutton <50450947+Ioome@users.noreply.github.com>
2022-10-26 11:42:06 +0800

    sutton

--- leetcode/src/main/java/com/sutton/leetcode/TwoNumber/leetTwoNumber.java
+++ leetcode/src/main/java/com/sutton/leetcode/TwoNumber/leetTwoNumber.java
@@ -5,7 +5,7 @@ package com.sutton.leetcode.TwoNumber;
  * @program: nirvana
  * @description: LeetCode 第一题：两数之和
  * 1.条件: 整数数组 nums和 一个整数目标值 target
- * 2.功能: 查找
+ * 2.功能: 查找⭐
  * 3.功能详情:  从数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标
  * 4.目标: 返回下标
  * 5.额外条件:你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现.你可以按任意顺序返回答案。
@@ -14,7 +14,17 @@ package com.sutton.leetcode.TwoNumber;
  * @create: 2022-10-26 11:36
  **/
 public class leetTwoNumber {
+
+    /**
+     * *
+     * @param aimArray 数据数组
+     * @param target   目标
+     */
     public static void getArrayTwoIndex(int[] aimArray,int target){
 
+    }
+
+    public static void main(String[] args) {
+        getArrayTwoIndex(new int[]{1,2,3,4,5,6},5);
     }
 }
