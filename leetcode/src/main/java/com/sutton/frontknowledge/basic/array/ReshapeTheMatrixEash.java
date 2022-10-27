57fb7a4bd6aeff7486e59d66457431d7ca80d994
sutton <50450947+Ioome@users.noreply.github.com>
2022-10-27 16:27:47 +0800

    sutton

---
+++ leetcode/src/main/java/com/sutton/frontknowledge/basic/array/ReshapeTheMatrixEash.java
@@ -0,0 +1,23 @@
+package com.sutton.frontknowledge.basic.array;
+
+/**
+ * @version 1.0.1
+ * @program: nirvana
+ * @description: 改变矩阵
+ * Input:
+ * ·····nums =
+ * ·····[[1,2],
+ *  ·····[3,4]]
+ * ·····r = 1, c = 4
+ *
+ *     Output:
+ *        [[1,2,3,4]]
+ *
+ * Explanation:
+ * The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+ *
+ * @author: Mr.wang.sutton
+ * @create: 2022-10-27 12:10
+ **/
+public class ReshapeTheMatrixEash {
+}
