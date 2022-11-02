aada789fd487ec20b63fd96a878bd026d9efe351
sutton <50450947+Ioome@users.noreply.github.com>
2022-11-02 11:34:04 +0800

    sutton

--- javaskills/src/main/java/org/example/hashmapiteration/iterationUtil.java
+++ javaskills/src/main/java/org/example/hashmapiteration/iterationUtil.java
@@ -1,8 +1,23 @@
 package org.example.hashmapiteration;
 
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @version 1.0.1
@@ -11,6 +26,12 @@ import java.util.Map;
  * @author: Mr.wang.sutton
  * @create: 2022-11-02 10:23
  **/
+@BenchmarkMode(Mode.AverageTime) // 测试完成时间
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS) // 预热 2 轮，每次 1s
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS) // 测试 5 轮，每次 1s
+@Fork(1) // fork 1 个线程
+@State(Scope.Thread) // 每个测试线程一个实例
 public class iterationUtil {
     /**
      * 今天的主角
@@ -47,6 +68,7 @@ public class iterationUtil {
     /**
      * 使用迭代器（Iterator）EntrySet 的方式进行遍历；
      */
+    @Benchmark
     public void useEntrySet() {
         /**
          * 相当于获取了一个HashMap 元素，并且  Iterator 指向了 它.
@@ -63,6 +85,7 @@ public class iterationUtil {
      * 使用迭代器（Iterator）KeySet 的方式进行遍历；
      * 此方法是获取 key 的方式,
      */
+    @Benchmark
     public void useKeySet() {
         Iterator<Integer> iterator = map.keySet().iterator();
         while (iterator.hasNext()) {
@@ -76,6 +99,7 @@ public class iterationUtil {
      * 使用 For Each EntrySet 的方式进行遍历
      * 原理也是通过获取 hash 元素去处理
      */
+    @Benchmark
     public void useForEach() {
         for (Map.Entry<Integer, String> entry : map.entrySet()) {
             System.out.println(entry.getKey());
@@ -87,6 +111,8 @@ public class iterationUtil {
      * 使用 For Each KeySet 的方式进行遍历；
      * 获取到 key
      */
+    @Benchmark
+
     public void useForEachEntryKey() {
         for (Integer key : map.keySet()) {
             System.out.println(key);
@@ -97,6 +123,8 @@ public class iterationUtil {
     /**
      * 使用 Lambda 表达式的方式进行遍历；
      */
+    @Benchmark
+
     public void useLambda() {
         map.forEach((key, value) -> {
             System.out.println(key);
@@ -107,6 +135,7 @@ public class iterationUtil {
     /**
      * 使用 Streams API 单线程的方式进行遍历；
      */
+    @Benchmark
     public void useStream() {
         map.entrySet().stream().forEach((entry) -> {
             System.out.println(entry.getValue());
@@ -117,6 +146,7 @@ public class iterationUtil {
     /**
      * 使用 Streams API 多线程的方式进行遍历。
      */
+    @Benchmark
     public void useParallelStream() {
         map.entrySet().parallelStream().forEach((entry) -> {
             System.out.println(entry.getKey());
@@ -124,9 +154,17 @@ public class iterationUtil {
         });
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RunnerException {
         iterationUtil util = new iterationUtil();
         util.assignment();
         util.useStream();
+
+        // 启动基准测试
+        Options opt = new OptionsBuilder()
+                // 要导入的测试类
+                .include(iterationUtil.class.getSimpleName())
+                .output("jmh-map.log") // 输出测试结果的文件
+                .build();
+        new Runner(opt).run(); // 执行测试
     }
 }
