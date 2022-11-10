1d618def6b3da9b25dd87113db9d7f6b8c87557a
sutton <50450947+Ioome@users.noreply.github.com>
2022-11-10 17:40:53 +0800

    sutton

---
+++ javaskills/src/main/java/org/example/usejdbc/Main.java
@@ -0,0 +1,14 @@
+package org.example.usejdbc;
+
+/**
+ * @version 1.0.1
+ * @program: nirvana
+ * @description: 学习 JDBC  主类
+ * @author: Mr.wang.sutton
+ * @create: 2022-11-10 17:40
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
